08a1f238b065b8d5be17ffbbddef7e23fdf50c74
Nabin <nabindenepali@outlook.com>
2020-06-03 10:06:58 +0545

    Change problem #1 to use BigNumber

--- 001/sum_of_multiples.js
+++ 001/sum_of_multiples.js
@@ -1,12 +1,19 @@
+const BigNumber = require('bignumber.js');
+
+BigNumber.prototype.floor = function () {
+    return this.integerValue(BigNumber.ROUND_FLOOR);
+};
+
+
 console.time('computation');
 
 /***
  * Calculate the sum of the natural numbers up to the specified number
  * @param num - Number upto which the natural numbers have to be added
- * @returns {number} - Sum of the natural numbers
+ * @returns {BigNumber} - Sum of the natural numbers
  */
 function sumOfNaturalNumbers(num) {
-    return num*(num+1)/2;
+    return num.times(num.plus(1).dividedBy(2));
 }
 
 /***
@@ -15,12 +22,13 @@ function sumOfNaturalNumbers(num) {
  * @param limit - A limit below which the multiples are to be summed
  */
 function sumOfMultiples(num, limit) {
-    var maxMultiplier = Math.floor((limit-1)/num);
-    console.log(maxMultiplier);
-    return num*sumOfNaturalNumbers(maxMultiplier);
+    var maxMultiplier = limit.minus(1).dividedBy(num).floor();
+    return sumOfNaturalNumbers(maxMultiplier).times(num);
 }
 
 // Numbers like 15, 30, ... appear as the multiples of both 3 and 5
-console.log(sumOfMultiples(3, 1000) + sumOfMultiples(5, 1000) - sumOfMultiples(15, 1000));
+const below = new BigNumber(1000);
+const sum = sumOfMultiples(3, below).plus(sumOfMultiples(5, below)).minus(sumOfMultiples(15, below));
+console.log(sum.toString());
 
 console.timeEnd('computation');
